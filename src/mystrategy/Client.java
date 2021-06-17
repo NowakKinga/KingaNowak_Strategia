b66e0f09ddd6804c65fe32cf4594e7445069d911
Kinga Nowak <kinga.nowak@dreamstormstudios.com>
2021-06-17 23:26:00 +0200

    Added using method for strategy in client code

--- src/mystrategy/Client.java
+++ src/mystrategy/Client.java
@@ -6,6 +6,13 @@
 package mystrategy;
 
 import data.DataGenerator;
+import java.util.Arrays;
+import sort.methods.Bubblesort;
+import sort.methods.Insertionsort;
+import sort.methods.Quicksort;
+import sort.methods.Selectionsort;
+import sort.strategy.Context;
+import sort.strategy.ISortStrategy;
 
 /**
  *
@@ -21,10 +28,33 @@ public class Client {
         /* Algorytmy sortowania */
         // http://www.algorytm.org/algorytmy-sortowania/
         
-        double[] dataNonSort = DataGenerator.generate(100000);
+        double[] dataNonSort = DataGenerator.generate(100000); //liczbę 100000 można zmienić na inną dowolną
         
        /* Wzorzec Stratega */
         //...
+       //Pierwsza metoda sortowania
+       Context sortContext = new Context(new Selectionsort()); //selecting the sort method
+       sortContext.arrange(dataNonSort);
+       double time = sortContext.sortedDoubleTab(dataNonSort);
+       Arrays.stream(dataNonSort).forEach( x -> System.out.println(x));
+       
+       //Druga metoda sortowania
+       Context sortContext1 = new Context((ISortStrategy) new Bubblesort());
+       sortContext1.arrange(dataNonSort);
+       double time1 = sortContext1.sortedDoubleTab(dataNonSort);
+       Arrays.stream(dataNonSort).forEach( x -> System.out.println(x));
+       
+       //Trzecia metoda sortowania
+       Context sortContext2 = new Context(new Insertionsort());
+       sortContext2.arrange(dataNonSort);
+       double time2 = sortContext2.sortedDoubleTab(dataNonSort);
+       Arrays.stream(dataNonSort).forEach( x -> System.out.println(x));
+       
+       //Czwarta metoda sortowania
+       Context sortContext3 = new Context((ISortStrategy) new Quicksort());
+       sortContext3.arrange(dataNonSort);
+       double time3 = sortContext3.sortedDoubleTab(dataNonSort);
+       Arrays.stream(dataNonSort).forEach( x -> System.out.println(x));
        
         System.out.println("Time: ??");
     }
